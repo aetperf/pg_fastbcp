8aa546d5e7889f7afa4300da1fb65ef196ff5834
PierreAntoineAP <pierre-antoine.collet@architecture-performance.fr>
2025-09-08 15:40:18 +0200

    return table pg_fastbcp

--- pg_fastbcp--1.0.sql
+++ pg_fastbcp--1.0.sql
@@ -41,7 +41,7 @@ RETURNS TABLE (
     output text,
     total_rows bigint,
     total_columns integer,
-    total_time_ms bigint    
+    total_time bigint    
 ) AS 'pg_fastbcp','xp_RunFastBcp_secure'
 LANGUAGE C;
 
